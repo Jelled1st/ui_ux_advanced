cc2032c578e5565d3394bb4d2281cf2c8e78bdb4
iVendrix <58091030+iVendrix@users.noreply.github.com>
2020-01-28 00:19:41 +0100

    new scenes

---
+++ UI_UX Advanced/Assets/Scripts/WebURLScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WebURLScript : MonoBehaviour
+{
+
+    public string Url;
+
+    public void Open()
+    {
+        Application.OpenURL("https://account.saxion.nl/");
+    }
+    
+
+}
