4ef168c583f752410f990ea81a9d249aa26b5ab0
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-01-28 23:56:10 +0100

    Fixed weird camera in Emanuel's scene

---
+++ UI_UX Advanced/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonInput : MonoBehaviour
+{
+    public void DebugPress()
+    {
+        Debug.Log("Debug press!");
+    }
+
+    public void SwitchToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
